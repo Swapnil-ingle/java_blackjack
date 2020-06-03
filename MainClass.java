53b5286b160d06b7b342efda9ae46acc5f5a78cc
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2020-06-03 18:31:33 +0530

    Fixed Issues

--- MainClass.java
+++ MainClass.java
@@ -1,25 +1,6 @@
-<<<<<<< HEAD
-class MainClass {
-
-    public static void main(String arg[]) {
-
-        BlackJackGame game = new BlackJackGame();
-<<<<<<< HEAD
-		game.play();
-=======
-	game.play();
->>>>>>> DRIVER_BLACKJACK_IMPL
-
-    }
-}
-
-||||||| merged common ancestors
-=======
 class MainClass {
     public static void main(String arg[]) {
 		BlackJackGame game = new BlackJackGame();
 		game.play();
     }
 }
-
->>>>>>> DRIVER_BLACKJACK_IMPL
